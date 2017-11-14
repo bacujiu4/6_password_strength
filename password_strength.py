5fcc75319c196bae8b17592733381625f2ad670b
bacujiu4 <1984skv@gmail.com>
2017-11-14 17:03:42 +0300

    edit

--- password_strength.py
+++ password_strength.py
@@ -5,4 +5,8 @@ def get_password_strength(password):
 
 
 if __name__ == '__main__':
-    pass
+    s = 'megaPaSSw#$Ord'
+    for symbol in s:
+        print('isupper', symbol, s.isupper())
+        print('islower', symbol, s.islower())
+        print(symbol, s.isprintable())
